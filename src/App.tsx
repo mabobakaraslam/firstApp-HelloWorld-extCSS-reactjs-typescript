aa8709d55dedfeb8b674d49044066ffe9077ae37
mabobakaraslam <m.abobakar.aslam@gmail.com>
2026-05-01 19:31:39 +0500

    code updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 // File: App.tsx located in app-name/src/
 
+import "./App.css";
+
 export default function App() {
-  return <h1>Hello World</h1>;
+  return <h1 className="appcenter" style={{color: "red"}}>Hello World</h1>;
 }
